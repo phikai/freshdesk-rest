c087112cabc082b3e576bf5e373f8069b3889c9e
Elias Van Ootegem <elias@wimionline.be>
2014-07-08 18:50:44 +0200

    Add initial UNTEST code for customField support

---
+++ src/Freshdesk/Model/CustomField.php
@@ -0,0 +1,137 @@
+<?php
+namespace Freshdesk\Model;
+
+use \Traversable;
+
+class CustomField extends Base
+{
+    const RESPONSE_KEY = '_\d+';//use to remove numeric appendix from field-names
+
+    /**
+     * @var string
+     */
+    protected $numericAppendix = null;
+
+    /**
+     * @var string
+     */
+    protected $name = null;
+
+    /**
+     * @var mixed
+     */
+    protected $value = null;
+
+    /**
+     * @var \Freshdesk\Model\Ticket
+     */
+    protected $ticket = null;
+
+    /**
+     * @param \Traversable $obj
+     * @return $this
+     */
+    public function setByTraversable(Traversable $obj)
+    {
+        if (!$obj instanceof Ticket)
+            return parent::setByTraversable($obj);
+        $custom = $obj->getCustomField();
+        $fields = array();
+        foreach ($custom as $k => $v)
+        {
+            $fields[] = new CustomField(
+                array(
+                    'name'      => $k,
+                    'value'     => $v,
+                    'ticket'    => $obj
+                )
+            );
+        }
+        array_pop($fields);
+        $fields[] = $this->setName($k)
+            ->setValue($v)
+            ->setTicket($obj);
+        $obj->setCustomField($fields);
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        if (preg_match(self::RESPONSE_KEY, $name, $match))
+        {
+            $this->numericAppendix = $match[0];
+            $name = str_replace(
+                $this->numericAppendix,
+                '',
+                $name
+            );
+        }
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param bool $full = false
+     * @return string
+     */
+    public function getName($full = false)
+    {
+        if ($full === true)
+            $this->name.$this->numericAppendix;
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $mix
+     * @return $this
+     */
+    public function setValue($mix)
+    {
+        $this->value = $mix;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param \Freshdesk\Model\Ticket $mod
+     * @return $this
+     */
+    public function setTicket(Ticket $mod)
+    {
+        $this->ticket = $mod;
+        return $this;
+    }
+
+    /**
+     * @return \Freshdesk\Model\Ticket
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
+    /**
+     * @return string
+     */
+    public function toJsonData()
+    {
+        return json_encode(
+            array(
+                'custom_field'  => array(
+                    $this->name => $this->value
+                )
+            )
+        );
+    }
+}
