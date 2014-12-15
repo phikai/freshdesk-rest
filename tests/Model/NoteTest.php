8705c32d6516ec15b6531ffc6bf63420456f2173
Elias Van Ootegem <eliasvo@gmail.com>
2014-12-15 22:23:25 +0100

    Adding some more tests, just for the sake of testing...

--- tests/Model/NoteTest.php
+++ tests/Model/NoteTest.php
@@ -26,6 +26,58 @@ class NoteTest extends PHPUnit_Framework_TestCase
         }
     }
 
+    /**
+     * @dataProvider datePropertiesProvider
+     */
+    public function testDateProperties(array $values)
+    {
+        $note = new Note($values);
+        $instance = new Note();
+        if (isset($values['createdAt'])) {
+            $this->assertInstanceOf('DateTime', $note->getCreatedAt(false));
+            $this->assertEquals($values['createdAt'], $note->getCreatedAt());
+            $instance->setCreatedAt(new DateTime($values['createdAt']));
+            $this->assertEquals($values['createdAt'], $instance->getCreatedAt());
+        }
+        if (isset($values['updatedAt'])) {
+            $this->assertInstanceOf('DateTime', $note->getUpdatedAt(false));
+            $this->assertEquals($values['updatedAt'], $note->getUpdatedAt());
+            $instance->setUpdatedAt(new DateTime($values['updatedAt']));
+            $this->assertEquals($values['updatedAt'], $instance->getUpdatedAt());
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function datePropertiesProvider()
+    {
+        return array(
+            array(
+                array(
+                    'createdAt' => '2014-01-02 03:04:05',
+                    'updatedAt' => '2014-11-12 13:14:15'
+                )
+            ),
+            array(
+                array(
+                    'updatedAt' => '2014-11-12 13:14:15'
+                )
+            ),
+            array(
+                array(
+                    'createdAt' => '2014-01-02 03:04:05'
+                )
+            ),
+            array(
+                array(
+                    'createdAt' => '2013-01-02 03:04:05',
+                    'updatedAt' => '2014-11-12 13:14:15'
+                )
+            )
+        );
+    }
+
     public function testSetAll()
     {
         $getters = array();
@@ -73,7 +125,9 @@ class NoteTest extends PHPUnit_Framework_TestCase
                         0, -6
                     );
                     $this->assertEquals($v, $note->{$getter}(false)->format('c'));
+                    $this->assertEquals($v, $setAll->{$getter}(false)->format('c'));
                     $this->assertEquals($ymdhis, $setAll->{$getter}(), $ymdhis);
+                    $this->assertEquals($ymdhis, $note->{$getter}(), $ymdhis);
                 } else {
                     $this->assertEquals($v, $note->{$getter}());
                     $this->assertEquals($v, $setAll->{$getter}());
